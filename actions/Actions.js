b85a0950d5b37f7260376ff4ba6f21c5bef67152
Richard Leagh <Richard@Richards-MBP.home>
2016-07-18 00:36:04 -0400

    Updating Structure Part 2

--- actions/Actions.js
+++
@@ -1,9 +0,0 @@
-const actions = {};
-
-const SELECT_POKEMON = 'SELECT_POKEMON';
-const CALCULATE = 'CALCULATE';
-
-actions.SELECT_POKEMON = SELECT_POKEMON;
-actions.CALCULATE = CALCULATE;
-
-export default actions;
--- components/Header.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Header = () => (
-	<h1 className="centeredHeader">Evolution Optimizer</h1>
-);
-
-export default Header;
